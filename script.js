9c32ff53ed5911985474b9886795b94af6f7f972
ioermolaev <ioermolaev1606@gmail.com>
2023-04-04 18:05:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Здесь можно добавить JavaScript код, если он понадобится в дальнейшем.
+document.addEventListener("DOMContentLoaded", function () {
+    let modal = document.getElementById("myModal");
+    let images = document.querySelectorAll(".product img");
+    let modalImg = document.getElementById("modalImage");
+  
+    images.forEach((img) => {
+      img.addEventListener("click", function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+      });
+    });
+  
+    let span = document.getElementsByClassName("close")[0];
+    span.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+  });
+  
